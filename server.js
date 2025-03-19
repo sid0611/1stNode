b116b59e1b4b7b35731347f0e74e9fbeea41e651
Siddharth Chaturvedi <chaturvedisiddharth11@gmail.com>
2025-03-19 22:13:10 +0530

    Added Patch Request

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express=require('express')
+const express=require('express');
 const app=express();
 app.use(express.json());
 
-let tasks=[];
 app.get('/tasks',(req,res)=>{
     res.json(tasks);
 })
-app.post('/tasks', (req, res) => {
-    console.log("Received Body:", req.body); // Debugging log
 
-    const task = req.body.task;
-    if (!task) {
-        console.log("Task is missing!"); // Debugging log
-        return res.status(400).json({ message: "Task is Required" });
-    }
-
+app.post('/tasks',(req,res)=>{
+    const task=req.body.task;
+    if(!task)res.status(400).json({message:"Task is required"});
     tasks.push(task);
-    console.log("Updated Tasks List:", tasks); // Debugging log
+    res.json({message:"Task added Successfully"});
+})
 
-    res.json({ message: "Task Added Successfully" });
-});
+app.patch('/tasks/:index',(req,res)=>{
+    const index=parseInt(req.params.index);
+    if(isNaN(index) || index<0 || index>=tasks.length)return res.status(400).json({messgae:"Invalid task Index"});
+    tasks.splice(index,1);
+    res.json({messgae:Task deleted Successfully});
+})
+
 const PORT=3000;
-app.listen(PORT,()=>
-    console.log(`Server started at ${PORT}`)
-)
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`);
+})
 
